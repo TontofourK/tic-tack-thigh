0df9957c5014ebb8d9588cfb02ff925f28bc5910
TontofourK <kapish21csu263@ncuindia.edu>
2024-01-29 11:35:43 +0530

    resume

--- js/script.js
+++ js/script.js
@@ -12,6 +12,41 @@ const App = {
         moves: [],
     },
     
+    getGameStatus(moves){
+        const p1moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId)
+        const p2moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId)
+        
+        console.log({p1moves})
+
+        const winningPatterns = [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        let winner = null
+
+        winningPatterns.forEach(pattern => {
+
+            console.log({pattern, p1moves, p2moves});
+            const p1Wins = pattern.every(v => p1moves.includes(v))
+            const p2Wins = pattern.every(v => p2moves.includes(v))
+        
+            if(p1Wins) winner = 1
+            if(p2Wins) winner = 2
+        })
+
+        return{
+            status: moves.length === 9 || winner != null ? 'complete' : 'in-progress',
+            winner // 1 | 2 | null
+        }
+    },
+
     init(){
         App.registerEventListners()
         
@@ -75,17 +110,16 @@ const App = {
                 square.replaceChildren(icon);
 
                 //check if the game ends
-                const winningPatterns = [
-                    [1, 2, 3],
-                    [1, 5, 9],
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [3, 5, 7],
-                    [3, 6, 9],
-                    [4, 5, 6],
-                    [7, 8, 9],
-                ];
+                const game = App.getGameStatus(App.state.moves);
 
+                if(game.status == 'complete'){
+                    if(game.winner){
+                        alert('Player %{game.winner} has won!');
+                    } else{
+                        alert('Tie!');
+                    }
+                }
+        
             });
         });
     },
